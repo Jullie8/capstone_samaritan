8d6d1ba7797ebb3477b4727f62a9e7e2920819df
Guzal Latypova <guzalic@gmail.com>
2018-04-04 22:20:29 -0400

    express app and sql

--- backend/charityInfo.sql
+++ backend/charityInfo.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS charityinfo;
+CREATE DATABASE charityinfo;
+
+\c charityinfo;
+
 DROP TABLE IF EXISTS charities;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS user_history;
@@ -18,3 +23,5 @@ CREATE TABlE user_history (
     USERID INTEGER,
     Create_Time TIMESTAMP
 );
+
+-- psql -f charityInfo.sql
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,13 @@
 var express = require('express');
 var router = express.Router();
+let db = require('../db/queries');
 
 ///users/:user_id/charities
 
 // Add charity 
-router.post('/:user_id/charities', function(req, res, next) {
 
-  db.any(`INSERT INTO user_history (user_id, create_time, charityName, URL) VALUES ( ${req.body.user_id}, now(), ${req.body.charityName}, ${req.body.URL})`)
-  .then(data => {
-    res.json(data);
-  })
-  .catch(err => console.log(err));
-});
+router.post('/:user_id/charities', db.addCharityLike);
+
+router.post('/:user_id/', db.addNewUser);
 
 module.exports = router;
